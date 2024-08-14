b9912b968a941ca59820cd0e46e58190de03b876
yamiltauil <ytauil@itcrowdarg.com>
2024-08-14 16:01:28 -0300

    fix styles and mock data

--- src/components/ProductComponents/LandingCard.tsx
+++ src/components/ProductComponents/LandingCard.tsx
@@ -1,15 +1,44 @@
 import Image from "next/image";
-function LandingCard() {
+import SlidingPane from "react-sliding-pane";
+import React, { useState } from "react";
+import SelectedProductCard from "./SelectedProductCard";
+
+function LandingCard({ product }: any) {
+  const [isProductViewOpen, setIsProductViewOpen] = useState<boolean>(false);
+  const [selectedImage, setSelectedImage] = useState(0);
   return (
-    <article className="h-[412px] w-[260px] flex flex-col justify-center font-helvetica m-auto hover:cursor-pointer">
-      <Image src="/Imagen pegada.png" alt="landing" width={260} height={412} />
-      <div className="flex flex-col justify-left m-auto">
-        <strong>$5000</strong>
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </p>
-      </div>
-    </article>
+    <>
+      <article
+        className="min-h-[412px] w-[260px] flex flex-col justify-center font-helvetica m-auto hover:cursor-pointer "
+        onClick={() => setIsProductViewOpen(true)}
+      >
+        <Image
+          src={product.image[selectedImage]}
+          alt={product.name}
+          width={260}
+          height={412}
+        />
+        <div className="flex flex-col justify-left left-0 max-w-[260px] w-full overflow-hidden">
+          <strong>{product.price}</strong>
+          <p className="text-sm">{product.name}</p>
+        </div>
+      </article>
+      <SlidingPane
+        className="z-40"
+        closeIcon={<div>X</div>}
+        isOpen={isProductViewOpen}
+        title={product.name}
+        width="800px"
+        onRequestClose={() => setIsProductViewOpen(false)}
+      >
+        <SelectedProductCard
+          product={product}
+          setIsProductViewOpen={setIsProductViewOpen}
+          selectedImage={selectedImage}
+          setSelectedImage={setSelectedImage}
+        />
+      </SlidingPane>
+    </>
   );
 }
 

24fafe7bbd87c612579dddc3b1c717e2da293f1b
Chmod351 <yamil.t.351@proton.me>
2024-08-16 20:10:01 -0300

    rename button and add styles

--- src/components/Ui/SubmitButton.tsx
+++ src/components/Ui/SubmitButton.tsx
@@ -6,7 +6,7 @@ export default function SubmitButton({ ...props }: ButtonFormProps) {
       type={props.type}
       disabled={props.disabled}
       onClick={props.onClick}
-      className="bg-black hover:bg-white hover:text-black hover:border-black hover:border-2 border-black border-2 text-white font-bold py-2 px-4 rounded-sm w-full h-14"
+      className="bg-black hover:bg-white hover:text-black hover:border-black hover:cursor-pointer hover:border-2 border-black border-2 text-white font-bold py-2 px-4 rounded-xl w-full  h-20"
     >
       {props.label}
     </button>

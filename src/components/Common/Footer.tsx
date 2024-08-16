72234791a23df84e66c86165ee49add11de4e0be
Chmod351 <yamil.t.351@proton.me>
2024-08-16 20:09:10 -0300

    improve styles

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -5,52 +5,57 @@ import { FaTiktok } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { SiMercadopago } from "react-icons/si";
 import { BsBank2 } from "react-icons/bs";
+import InfoBar from "@/components/Ui/InfoBar";
 
 export default function Footer() {
   return (
-    <footer className="bg-primary w-full  font-helvetica md:h-[406px] md:py-0 pt-10">
-      <div className="md:max-w-[1100px] w-11/12 m-auto flex md:justify-between items-center h-full space-x-4 md:flex-row flex-col-reverse  ">
-        <div className="w-[300px] h-[300px] bg-gray-400 md:p-0 my-10">
-          imagen
-        </div>
-        <ul className="flex flex-col space-y-4">
-          {data.map((item) => (
-            <li key={item.name} className="m-auto md:m-0">
-              <NextLink href={item.url}>{item.name}</NextLink>
-            </li>
-          ))}
-        </ul>
-        <ul className="flex flex-col space-y-4 ">
-          {data.map((item) => (
-            <li key={item.name} className="m-auto md:m-0">
-              <NextLink href={item.url}>{item.name}</NextLink>
-            </li>
-          ))}
-          <div className="flex md:space-x-4 space-x-8 items-center">
-            <div className="w-8 h-8 ">
-              <SiMercadopago className="w-full h-full" />
-            </div>
-            <div className="w-8 h-8 ">
-              <BsBank2 className="w-full h-full" />
-            </div>
+    <footer>
+      <footer className="bg-primary w-full  font-helvetica md:h-[406px] md:py-0 pt-10">
+        <div className="md:max-w-[1100px] w-11/12 m-auto flex md:justify-between items-center h-full space-x-4 md:flex-row flex-col-reverse  ">
+          <div className="w-[300px] h-[300px] bg-gray-400 md:p-0 my-10">
+            imagen
           </div>
-        </ul>
-        <div className="flex flex-col w-[300px]">
-          <div className="flex row md:space-x-4 space-x-8 py-2 md:justify-end justify-center">
-            <div className="w-8 h-8 ">
-              <FaTiktok className="w-full h-full" />
+          <ul className="flex flex-col space-y-4">
+            {data.map((item) => (
+              <li key={item.name} className="m-auto md:m-0">
+                <NextLink href={item.url}>{item.name}</NextLink>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex flex-col space-y-4 ">
+            {data.map((item) => (
+              <li key={item.name} className="m-auto md:m-0">
+                <NextLink href={item.url}>{item.name}</NextLink>
+              </li>
+            ))}
+            <div className="flex md:space-x-4 space-x-8 items-center">
+              <div className="w-8 h-8 ">
+                <SiMercadopago className="w-full h-full" />
+              </div>
+              <div className="w-8 h-8 ">
+                <BsBank2 className="w-full h-full" />
+              </div>
             </div>
-            <div className="w-8 h-8 ">
-              <FaInstagram className="w-full h-full" />
+          </ul>
+          <div className="flex flex-col w-[300px]">
+            <div className="flex row md:space-x-4 space-x-8 py-2 md:justify-end justify-center">
+              <div className="w-8 h-8 ">
+                <FaTiktok className="w-full h-full" />
+              </div>
+              <div className="w-8 h-8 ">
+                <FaInstagram className="w-full h-full" />
+              </div>
             </div>
+            <p>
+              Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum
+              dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit
+              ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum
+            </p>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum
-            dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit
-            ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum
-          </p>
         </div>
-      </div>
+      </footer>
+      <InfoBar />
     </footer>
   );
 }

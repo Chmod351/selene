34f4e8d5bd29a331353b19253062e2430597953c
Chmod351 <yamil.t.351@proton.me>
2024-08-17 10:13:05 -0300

    fix styles

--- src/components/Ui/LandingCardLoading.tsx
+++ src/components/Ui/LandingCardLoading.tsx
@@ -1,11 +1,13 @@
 export default function LandingCardLoading() {
   return (
-    <article className="h-[412px] w-[260px]    cursor-pointer">
-      <div className="bg-gray-400 animate-pulse h-[300px] w-full rounded-md" />
-      <div className="bg-white  h-[20px] w-full" />
+    <article className="h-[400px] w-[240px] cursor-pointer">
+      <div className="bg-gray-400 animate-pulse h-[240px] w-full rounded-md flex items-center justify-center">
+        <span className="text-center">Loading...</span>
+      </div>
+      <div className="bg-white h-[20px] w-full" />
       <div className="bg-gray-600 animate-pulse h-[20px] w-[80px] rounded-md" />
       <div className="w-full bg-white h-[14px]" />
-      <div className="bg-gray-400 animate-pulse h-[14px] w-full  rounded-md " />
+      <div className="bg-gray-400 animate-pulse h-[14px] w-full rounded-md" />
       <div className="w-full bg-white h-[14px]" />
       <div className="bg-gray-400 animate-pulse h-[14px] w-full rounded-md" />
     </article>

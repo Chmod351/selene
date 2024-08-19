573a591ea248d1a4f14898afa4da014edb62e7d8
Chmod351 <yamil.t.351@proton.me>
2024-08-19 19:20:01 -0300

    add support to type

--- src/components/Ui/SubmitButton.tsx
+++ src/components/Ui/SubmitButton.tsx
@@ -6,7 +6,7 @@ export default function SubmitButton({ ...props }: ButtonFormProps) {
       <button
         type={props.type}
         disabled={props.disabled}
-        onClick={props.onClick}
+        onClick={props?.onClick}
         className="bg-gray-400   hover:cursor-pointer hover:border-2 border-gray-400 border-2 text-white font-bold py-2 px-4 rounded-xl w-full  h-20"
       >
         {props.label}
@@ -17,7 +17,7 @@ export default function SubmitButton({ ...props }: ButtonFormProps) {
     <button
       type={props.type}
       disabled={props.disabled}
-      onClick={props.onClick}
+      onClick={props?.onClick}
       className="bg-black hover:bg-white hover:text-black hover:border-black hover:cursor-pointer hover:border-2 border-black border-2 text-white font-bold py-2 px-4 rounded-xl w-full  h-20"
     >
       {props.label}

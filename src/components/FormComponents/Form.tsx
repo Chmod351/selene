3479db43b1aedc073aab9146a46e0c6afef6e8aa
Chmod351 <yamil.t.351@proton.me>
2024-08-19 19:19:46 -0300

    delete unused props

--- src/components/FormComponents/Form.tsx
+++ src/components/FormComponents/Form.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import checkFormSchema from "@/components/FormComponents/checkoutFormSchema";
 import PaymentInfo from "@/components/Ui/PaymentInfo";
+import SubmitButton from "@/components/Ui/SubmitButton";
 
 interface FormProps {
   children: React.ReactNode;
 }
 
-function Form({ children }: FormProps) {
+function Form({ children, setIsCheckoutForm }: FormProps) {
   const {
     register,
     handleSubmit,
@@ -16,44 +17,85 @@ function Form({ children }: FormProps) {
   } = useForm({
     resolver: zodResolver(checkFormSchema),
   });
-
-  const onSubmitHandler = (data: any) => {
-    console.log(data);
-  };
+  const [deliveryMode, setDeveliveryMode] = useState<string | null>(null);
 
   return (
     <div className="w-full bg-white flex flex-col justify-between m-auto rounded-xl items-center h-full mt-28 mb-10">
       {/* retiro de pedido */}
-      <div className="w-full my-10 bg-primary rounded-xl">
-        <div className="w-11/12 flex justify-evenly flex-col mx-auto py-8 gap-4">
-          <label className="font-helvetica">
-            <input type="radio" className="mr-2" />
-            Retirar por sucursal de correo
-          </label>
-          <label className="font-helvetica">
-            <input type="radio" className="mr-2" />
-            Envio estandar a domicilio
-          </label>
-          <label className="font-helvetica">
-            <input type="radio" className="mr-2" />
-            Moto mensajeria 24 hs - CABA
-          </label>
-          <label className="font-helvetica">
-            <input type="radio" name="radio" value="" className="mr-2" />
-            Moto mensajeria 24 hs - GBA
-          </label>
+      <div className="w-full my-10 bg-primary rounded-xl flex flex-row ">
+        <div className="w-1/2 ">
+          <div className="flex justify-evenly flex-col mx-auto py-8 gap-4 ml-8">
+            <label className="font-helvetica">
+              <input
+                type="radio"
+                className="mr-2"
+                name="deliveryMode"
+                value="Pickup"
+                {...register("deliveryMode")}
+                checked={deliveryMode === "Pickup"}
+                onChange={() => setDeveliveryMode("Pickup")}
+              />
+              Retirar por sucursal de correo
+            </label>
+            <label className="font-helvetica">
+              <input
+                type="radio"
+                className="mr-2"
+                name="deliveryMode"
+                value="Standard"
+                {...register("deliveryMode")}
+                checked={deliveryMode === "Standard"}
+                onChange={() => setDeveliveryMode("Standard")}
+              />
+              Envío estándar a domicilio
+            </label>
+            <label className="font-helvetica">
+              <input
+                type="radio"
+                className="mr-2"
+                name="deliveryMode"
+                value="Express_CABA"
+                {...register("deliveryMode")}
+                checked={deliveryMode === "Express_CABA"}
+                onChange={() => setDeveliveryMode("Express_CABA")}
+              />
+              Moto mensajería 24 hs - CABA
+            </label>
+            <label className="font-helvetica">
+              <input
+                type="radio"
+                className="mr-2"
+                name="deliveryMode"
+                value="Express_GBA"
+                {...register("deliveryMode")}
+                checked={deliveryMode === "Express_GBA"}
+                onChange={() => setDeveliveryMode("Express_GBA")}
+              />
+              Moto mensajería 24 hs - GBA
+            </label>
+          </div>
         </div>
+        <div className="w-1/2 animate-pulse h-52 bg-gray-600"></div>
       </div>
 
       <div className="w-full bg-white flex flex-col justify-between m-auto h-auto rounded-xl">
-        <form onSubmit={handleSubmit(onSubmitHandler)}>
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
           {React.Children.map(children, (child) => {
             return React.cloneElement(child as React.ReactElement<any>, {
               register,
+              isDisabled: !deliveryMode ? true : false,
               errors,
             });
           })}
-          <PaymentInfo />
+          <div className="w-full" onClick={() => setIsCheckoutForm(true)}>
+            <PaymentInfo>
+              <SubmitButton
+                disabled={deliveryMode ? false : true}
+                label="Confirmar pedido"
+                type="submit"
+              />
+            </PaymentInfo>
+          </div>
         </form>
       </div>
     </div>
--- src/components/FormComponents/checkoutFormSchema.ts
+++ src/components/FormComponents/checkoutFormSchema.ts
@@ -1,28 +1,25 @@
 import { z } from "zod";
-// i needs to validate string only with letters
+
+// Esquema de validación general para la orden
 const checkoutFormSchema = z.object({
-  firstName: z
+  commentaries: z.string().optional(),
+  shippingAddress1: z
     .string()
-    .min(1, { message: "El nombre es requerido" })
-    .max(20)
-    .regex(/^[a-zA-Z]+$/),
-  lastName: z
-    .string()
-    .min(1, { message: "El apellido es requerido" })
-    .max(20)
-    .regex(/^[a-zA-Z]+$/),
-  houseNumber: z.number().optional(),
-  floor: z
-    .string()
-    .regex(/^[a-zA-Z0-9\s,'-]+$/)
-    .optional(),
-  phoneNumber: z
-    .string()
-    .min(10, { message: "Número de teléfono debe tener al menos 10 dígitos" })
-    .regex(/^[0-9]+$/),
-  address: z
-    .string()
-    .min(1, { message: "Dirección es requerida" })
-    .regex(/^[a-zA-Z0-9\s,'-]+$/),
+    .min(1, { message: "La dirección  es requerida" }),
+  city: z.string().min(1, { message: "La ciudad es requerida" }),
+  country: z.string().min(1, { message: "El país es requerido" }),
+  state: z.string().min(1, { message: "El estado/provincia es requerido" }),
+  email: z.string().email({ message: "Debe ser un email válido" }),
+  user_Id: z.string().min(1, { message: "Este campo es requerido" }),
+  name: z.string().min(1, { message: "El nombre es requerido" }),
+  surname: z.string().min(1, { message: "El apellido es requerido" }),
+  floor: z.string().optional(),
+  phone: z.string().min(1, { message: "El telefón es requerido" }),
+  // phone2: z.string().optional(),
+  userIdCard: z
+    .string({ required_error: "El DNI es requerido" })
+    .min(1, { message: "El DNI es requerido" }),
+  zip: z.string().min(1, { message: "El código postal es requerido" }),
 });
+
 export default checkoutFormSchema;

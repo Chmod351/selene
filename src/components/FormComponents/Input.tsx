f1ae52540f21bbe27c34f2ce5f75535a38ce58af
Chmod351 <yamil.t.351@proton.me>
2024-08-20 11:53:43 -0300

    show err

--- src/components/FormComponents/Input.tsx
+++ src/components/FormComponents/Input.tsx
@@ -7,23 +7,35 @@ function InputField({
   errors,
   placeholder,
   type = "text",
+  disabled,
   required,
-}: any) {
+}: {
+  label: string;
+  name: string;
+  register: any;
+  errors: any;
+  placeholder: string;
+  type?: string;
+  disabled?: boolean;
+  required?: boolean;
+}) {
+  // Desestructuración del error específico del campo basado en el `name`
+  const error = errors?.[name.split(".").join("?.")]; // Accede al error anidado
+
   return (
     <div className="flex flex-col w-full ">
       <label className="font-helvetica text-sm hidden ">{label}</label>
       <input
-        className={` rounded p-4 mt-1 placeholder:text-black  outline-none  ${
-          errors[name] ? "border-red-500" : "border-gray-300"
+        className={`rounded p-4 mt-1 placeholder:text-black  outline-none ${
+          error ? "border-red-500" : "border-gray-300"
         }`}
         {...register(name, { required })}
         placeholder={placeholder}
         type={type}
+        disabled={disabled}
       />
-      {errors[name] && (
-        <span className="text-red-500 text-sm mt-1">
-          Este campo es obligatorio
-        </span>
+      {error && (
+        <span className="text-red-500 text-sm mt-1">{error.message}</span>
       )}
     </div>
   );

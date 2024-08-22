2056d6a8d8fbd1836fdd1b29eb871e4c5cc3a7db
Chmod351 <yamil.t.351@proton.me>
2024-08-22 07:53:55 -0300

    fix payment info

--- src/components/Ui/PaymentInfo.tsx
+++ src/components/Ui/PaymentInfo.tsx
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
 import EcommerceContext from "@/store/store";
 import { IProduct } from "@/components/ProductComponents/types";
-import SubmitButton from "@/components/Ui/SubmitButton";
 
-function PaymentInfo() {
+function PaymentInfo({ children }: any) {
   const { cart }: { cart: IProduct[] } = useContext(EcommerceContext);
   return (
     <div className="w-full min-h-[230px]  bg-primary rounded-xl font-helvetica py-8">
       <div className="w-11/12 flex items-center justify-center flex-col gap-4 m-auto">
         {cart.map((item) => (
-          <ul key={item.name_es}>
+          <ul key={item._id}>
             <span className="flex flex-row gap-4 text-2xl">
               <li>{item.name_es}</li>
               <span>${item.price_es}</span>
@@ -19,13 +18,7 @@ function PaymentInfo() {
         <strong className="text-3xl">
           Total: ${cart.reduce((a, b) => a + b.price_es, 0)}{" "}
         </strong>
-
-        <SubmitButton
-          type="submit"
-          label="Pagar"
-          onClick={() => {}}
-          disabled={true}
-        />
+        {children}
       </div>
     </div>
   );

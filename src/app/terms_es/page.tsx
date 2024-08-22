ec97bb26bd1b9f2742fa2060998dfa1552d4e523
Chmod351 <yamil.t.351@proton.me>
2024-08-22 07:53:22 -0300

    add privacy and toc pages

---
+++ src/app/terms_es/page.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function page() {
+  return (
+    <div>
+      Términos y Condiciones Última actualización: Fecha de actualización
+      Bienvenido a [Nombre de tu sitio/web/app]. Al utilizar nuestro servicio,
+      aceptas los siguientes términos y condiciones. Por favor, lee
+      detenidamente estos términos antes de continuar. Introducción Estos
+      Términos y Condiciones ("Términos") rigen el uso de [Nombre de tu
+      sitio/web/app] y cualquier producto o servicio asociado que ofrezca. Al
+      acceder o utilizar [Nombre de tu sitio/web/app], aceptas estos Términos
+      sin reservas. Definiciones Las palabras y frases que aparecen en cursiva
+      tienen los significados asignados a ellas en la presente sección. Cuentas
+      de Usuarios Al registrarte en [Nombre de tu sitio/web/app], debes
+      proporcionar información precisa y completa. Es responsable de mantener la
+      seguridad de tu cuenta y no compartir tu contraseña con nadie. Contenido
+      Reserva todos los derechos sobre el contenido disponible en [Nombre de tu
+      sitio/web/app], incluidos textos, gráficos, logos, imágenes, y software.
+      El uso no autorizado de dicho contenido constituye una violación de estas
+      Términos. Política de Derechos de Autor Todos los derechos de autor,
+      marcas, patentes, y derechos similares están protegidos por leyes
+      internacionales. No se permite copiar, distribuir, ni alterar contenido
+      sin permiso explícito. Propiedad Intelectual [Nombre de tu sitio/web/app]
+      posee todos los derechos sobre el software y el contenido proporcionado.
+      No se permite su uso fuera de los límites establecidos por estas Términos.
+      Retroalimentación de Usuarios Podemos solicitar retroalimentación sobre
+      nuestros productos o servicios. Al proporcionar dicha retroalimentación,
+      otorgas a [Nombre de tu sitio/web/app] una licencia gratuita para usarla.
+      Enlaces a Otros Sitios Web Los enlaces a otros sitios web son
+      proporcionados únicamente para tu conveniencia y no representan ninguna
+      afiliación o respaldo por nuestra parte. Terminación Podemos terminar
+      estos Términos en cualquier momento, notificándote por correo electrónico.
+      También puedes terminar estos Términos en cualquier momento cancelando tu
+      cuenta. Limitación de Responsabilidad No somos responsables de daños
+      indirectos o consecuentes derivados del uso de [Nombre de tu
+      sitio/web/app]. Ley Aplicable Estos Términos se rigen por las leyes de
+      [país/nacionalidad]. Resolución de Disputas Cualquier disputa relacionada
+      con estos Términos será resuelta por un tribunal competente en [ubicación
+      legal]. Cambios Reservamos el derecho de modificar estos Términos en
+      cualquier momento. Te notificaremos de cualquier cambio relevante.
+      Contacto Para cualquier consulta o aclaración, contacta con nosotros a
+      través de [información de contacto].
+    </div>
+  );
+}
+
+export default page;

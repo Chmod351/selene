6c41bb0ed380110e4c815e44f897d87a306f611e
Chmod351 <yamil.t.351@proton.me>
2024-08-27 13:21:41 -0300

    split types

--- src/components/ProductComponents/SelectedProductCard.tsx
+++ src/components/ProductComponents/SelectedProductCard.tsx
@@ -4,41 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import EcommerceContext from "@/store/store";
 import SubmitButton from "../Ui/SubmitButton";
 
-import { IProduct } from "@/components/ProductComponents/types";
-
-type Sizes =
-  | "XS"
-  | "S"
-  | "M"
-  | "L"
-  | "XL"
-  | "XXL"
-  | "XXXL"
-  | "XXXXL"
-  | "25"
-  | "26"
-  | "27"
-  | "28"
-  | "29"
-  | "30"
-  | "31"
-  | "32"
-  | "33"
-  | "34"
-  | "35"
-  | "36"
-  | "37"
-  | "38"
-  | "39"
-  | "40"
-  | "41"
-  | "42"
-  | "43"
-  | "44"
-  | "45"
-  | "46"
-  | "47"
-  | "48";
+import { IProduct, Sizes } from "@/components/ProductComponents/types";
 
 const fetchProductFromApi = async (productId: string) => {
   try {

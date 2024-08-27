02a704da6602f12bf668bd49aac9daa0339ba526
Chmod351 <yamil.t.351@proton.me>
2024-08-27 13:39:38 -0300

    add types

--- src/components/Ui/SearchBar.tsx
+++ src/components/Ui/SearchBar.tsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import { useIsQuerySearch } from "@/hooks/useIsQuerySearch";
 import SlidingPane from "react-sliding-pane";
 import { FaSearch } from "react-icons/fa";
-import { IProduct } from "@/components/ProductComponents/types";
 import { useUrl } from "@/store/UrlProvider";
 import Image from "next/image";
+import { IProduct } from "@/components/ProductComponents/types";
+import {
+  SearchResultsLogicProps,
+  SearchBarComponentProps,
+  SlidingPaneSearchProps,
+} from "@/components/Ui/types";
 
 function SearchResultsLogic({
   data,
@@ -12,13 +17,7 @@ function SearchResultsLogic({
   isLoading,
   setIsSearchOpen,
   setQuery,
-}: {
-  setQuery: React.Dispatch<React.SetStateAction<string | null>>;
-  data: IProduct[] | undefined;
-  error: Error | null;
-  setIsSearchOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  isLoading: boolean;
-}) {
+}: SearchResultsLogicProps) {
   const { setUrl } = useUrl();
 
   if (error) {
@@ -91,11 +90,7 @@ function SearchBarComponent({
   handleSearch,
   handleClickWithQuery,
   query,
-}: {
-  handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleClickWithQuery: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  query: string | null;
-}) {
+}: SearchBarComponentProps) {
   return (
     <form className="flex px-4 py-2 rounded-3xl bg-white md:w-[400px] w-full border-2 flex-row">
       <FaSearch className="w-6 h-6 text-[#999999] mr-2" />
@@ -118,14 +113,7 @@ function SlidingPaneSearch({
   handleClickWithQuery,
   query,
   children,
-}: {
-  setIsSearchOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  isSearchOpen: boolean;
-  handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleClickWithQuery: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  query: string | null;
-  children: React.ReactNode;
-}) {
+}: SlidingPaneSearchProps) {
   return (
     <SlidingPane
       closeIcon={<div>X</div>}

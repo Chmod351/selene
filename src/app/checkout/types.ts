2bc822432ac4e09991cb4b39b70f6f3da23fce2c
Chmod351 <yamil.t.351@proton.me>
2024-09-10 18:59:41 -0300

    add checkout types

--- src/app/checkout/types.ts
+++ src/app/checkout/types.ts
@@ -1,21 +1,26 @@
-import { IProduct } from "@/components/ProductComponents/types";
-interface OrderItems {
-  productId: IProduct;
-  productPrice: number;
-  quantity: number;
-  color: string;
-  size: string;
-}
-
 export interface IOrder {
   commentaries: string;
   deliveryMode: string;
-  orderItems: [OrderItems];
+  orderItems: [
+    {
+      _id: string;
+      price_es: number;
+      price_en: number;
+      name_es: string;
+      name_en: string;
+      description_es: string;
+      description_en: string;
+      productPrice: number;
+      quantity: number;
+      color: string;
+      size: string;
+    },
+  ];
   shippingAddress1: string;
   shippingAddress2: string;
   paymentMethod: string;
   paymentStatus: "Pending" | "Failed" | "Success";
-  status: "Pending" | "Processing" | "Shipped" | "Delivered" | "Cancelled";
+  // status: "Pending" | "Processing" | "Shipped" | "Delivered" | "Cancelled";
   totalPrice: number;
   userData: {
     city: string;
@@ -24,7 +29,7 @@ export interface IOrder {
     email: string;
     name: string;
     phone: string;
-    phone2: string;
+    // phone2: string;
     surname: string;
     zip: string;
   };

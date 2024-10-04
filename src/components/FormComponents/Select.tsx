d9442db1152b5e74bab2a31e370f15e537128164
Chmod351 <yamil.t.351@proton.me>
2024-10-03 21:31:27 -0300

    improve error handling

--- src/components/FormComponents/Select.tsx
+++ src/components/FormComponents/Select.tsx
@@ -1,24 +1,39 @@
 import { SelectFieldProps } from "@/components/FormComponents/types";
 export default function SelectField({ ...props }: SelectFieldProps) {
+  let error;
+  if (props.errors.isArray) {
+    error = props.errors?.[0]?.[props.name.split(".").join("?.")];
+    console.log("errrrrrrrrrrrr", error);
+  } else {
+    error = props.errors?.[props.name.split(".").join("?.")];
+  }
   return (
     <div className="flex flex-col gap-1 w-full font-helvetica  ">
       <label htmlFor={props.name}>{props.label}</label>
       <select
-        className="rounded  p-2"
+        className="rounded  p-4"
         id={props.name}
+        required={true}
+        {...props.register}
         name={props.name}
-        value={props.value}
         onChange={props.onChange}
         defaultValue={props.defaultValue}
       >
-        {props.options.map((option) => (
+        {props.options.map((option: { label: string; value: string }) => (
           <option key={option.value} value={option.value}>
             {option.label}
           </option>
         ))}
       </select>
-      <span className="text-red-500">{props.errors}</span>
       <br />
+      {error && (
+        <span className="text-red-500 text-sm mt-1">{error.message}</span>
+      )}
+      {props.errors?.stock?.length > 0 && props.name.startsWith("stock") && (
+        <span className="text-red-500 text-sm mt-1">
+          la informacion del stock debe estar completa
+        </span>
+      )}
     </div>
   );
 }
